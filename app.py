d89771aa6a7440d1154fe4619b3166c96d6b080e
Ferdi <ferdinand.popp@online.de>
2021-09-27 23:50:14 +0200

    removed logo

--- app.py
+++ app.py
@@ -34,10 +34,6 @@ def build_model(input_data):
     st.write(df)
     st.markdown(filedownload(df), unsafe_allow_html=True)
 
-# Logo image
-image = Image.open('images/logo.png')
-
-st.image(image, use_column_width=False)
 
 # Page title
 st.markdown("""
